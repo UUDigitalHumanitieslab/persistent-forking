af389a803570accf31f439846081cf9cc887fb1b
Julian Gonggrijp <j.gonggrijp@uu.nl>
2015-08-27 14:57:11 +0200

    Make indentation of template files more consistent

--- templates/public_fork_box.php
+++ templates/public_fork_box.php
@@ -1,19 +1,19 @@
 <?php
-    /**
-     * Render the inset with fork button and parent link for the 
-     * public view of a post.
-     *
-     * Required parameters:
-     * $post_id     the ID of the post for which the inset will be 
-     *              rendered
-     * $image_url   the URL of the fork icon image file
-     */
-    $fork_url = add_query_arg(array(
-        'action' => 'persistent_fork',
-        'post' => $post_id,
-        'nonce' => wp_create_nonce('persistent_forking')
-    ), home_url());
-    $parent_id = get_post_meta($post_id, '_persistfork-parent', true);
+/**
+ * Render the inset with fork button and parent link for the 
+ * public view of a post.
+ *
+ * Required parameters:
+ * $post_id     the ID of the post for which the inset will be 
+ *              rendered
+ * $image_url   the URL of the fork icon image file
+ */
+$fork_url = add_query_arg(array(
+    'action' => 'persistent_fork',
+    'post' => $post_id,
+    'nonce' => wp_create_nonce('persistent_forking')
+), home_url());
+$parent_id = get_post_meta($post_id, '_persistfork-parent', true);
 ?>
 <a href="<?= $fork_url ?>" title="Fork this post">
     <img
